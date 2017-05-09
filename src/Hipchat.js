bd6ccaaa221e40111f939268028194c449940463
mitogh <mitogh@gmail.com>
2017-05-09 10:53:34 -0500

    Allow to extend the default options

--- src/Hipchat.js
+++ src/Hipchat.js
@@ -1,47 +1,44 @@
 const fetch = require('node-fetch'); // polyfill
 const URL = require('url').URL;
 const URLSearchParams = require('url').URLSearchParams;
-const NOTIFICATIONS = require('./Notifications');
+const _ = require('lodash');
 var uniqid = require('uniqid');
 
 class Hipchat {
 
-  constructor( token, room_id_or_name, userParams ) {
+  constructor( token ) {
     // https://www.hipchat.com/docs/apiv2/method/send_room_notification
-    const defaultArgs = {
-      from: 'Lighthouse Reporter',
-      color: 'red',
-      card: {
-        id: uniqid(),
-        style: 'application',
-        url: "https://www.application.com/an-object",
-        format: 'compact',
-        title: "Lighthouse Report",
-        description: 'Your web page performance test has finished.',
-        icon: {
-          url: 'https://developers.google.com/web/progressive-web-apps/images/pwa-lighthouse.png'
-        },
-        attributes: [
-          {
-            label: 'Status',
-            value: {
-              label: 'In Progress',
-              style: 'lozenge-complete'
-            }
-          }
-        ],
-      }
-    }
-    this.room_id_or_name = room_id_or_name;
     this.token = token;
-    this.args = Object.assign({}, defaultArgs, userParams);
   }
 
   base () {
     return `https://api.hipchat.com/v2/room/${this.room_id_or_name}/notification`;
   }
 
-  send() {
+  send( options ) {
+    this.room_id_or_name = options.room;
+    this._setParams( options.args );
+    return this._request();
+  }
+
+  _setParams( userParams ) {
+    const defaultArgs = {
+      from: 'Lighthouse Reporter',
+      card: {
+        id: uniqid(),
+        style: 'application',
+        format: 'compact',
+        title: "Lighthouse Report",
+        icon: {
+          url: 'https://developers.google.com/web/progressive-web-apps/images/pwa-lighthouse.png'
+        },
+        attributes: [],
+      }
+    }
+    this.args = _.merge({}, defaultArgs, userParams);
+  }
+
+  _request() {
     const options = {
       method: 'POST',
       headers: {
@@ -50,7 +47,7 @@ class Hipchat {
       },
       body: JSON.stringify(this.args),
     }
-    console.log(options);
+
     return fetch( this.base(), options)
       .then( (response) => response.json() )
       .catch( (error) => {

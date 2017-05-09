85b7bf880a8ecccdd8c732f79fea73db403d237a
mitogh <mitogh@gmail.com>
2017-05-09 10:53:56 -0500

    Add reporter and notifications setup

--- src/Reporter.js
+++ src/Reporter.js
@@ -1,9 +1,31 @@
 const Report = require('./Report');
-const Hipchat = require('./Hipchat');
+const Notification = require('./Notification');
 
-function Reporter( request, reply ) {
-  console.log(request.query);
-  return reply({});
+async function Reporter( request, reply ) {
+
+  const result = await new Report({
+    k: process.env.WEB_PAGE_TEST_KEY || '',
+    url: request.query.site,
+  }).run();
+  
+  // Set error as default.
+  let notificationOptions = {
+    status: 'Error',
+    description: `There was an error proccesing ${request.query.site}`,
+    room: request.query.hipchat,
+    url: request.query.site,
+  }
+
+  // Update notificationOptions if was a success
+  if ( 'statusCode' in result && result.statusCode === 200 ) {
+    notificationOptions = Object.assign({}, notificationOptions, {
+      status: 'Started',
+      description: 'Your web page performance test has started.',
+      url: result.data.userUrl,
+    });
+  }
+  const notificationResult = await Notification(notificationOptions);
+  return reply(notificationResult);
 }
 
 module.exports = Reporter;

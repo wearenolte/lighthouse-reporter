2c2015148ba140d603dc2c30da6af4d1b71146b2
mitogh <mitogh@gmail.com>
2017-05-10 08:54:21 -0500

    Add Pinback URL for the verification process

--- src/Reporter.js
+++ src/Reporter.js
@@ -2,14 +2,16 @@ const Report = require('./Report');
 const Notification = require('./Notification');
 
 async function Reporter( request, reply ) {
-
+  // Params https://sites.google.com/a/webpagetest.org/docs/advanced-features/webpagetest-restful-apis
+  const base = process.env.BASE || 'http://localhost:8080';
   const result = await new Report({
-    k: process.env.WEB_PAGE_TEST_KEY || '',
+    k: process.env.WEB_PAGE_TEST_KEY || '', // k means KEY
     url: request.query.site,
+    pingback: `${base}/verification/?hipchat=${request.query.hipchat}`,
   }).run();
-  
+
   // Set error as default.
-  let notificationOptions = {
+  let notificationOpts = {
     status: 'Error',
     description: `There was an error proccesing ${request.query.site}`,
     room: request.query.hipchat,
@@ -18,13 +20,13 @@ async function Reporter( request, reply ) {
 
   // Update notificationOptions if was a success
   if ( 'statusCode' in result && result.statusCode === 200 ) {
-    notificationOptions = Object.assign({}, notificationOptions, {
+    let notificationOpts = Object.assign({}, notificationOpts, {
       status: 'Started',
       description: 'Your web page performance test has started.',
       url: result.data.userUrl,
     });
   }
-  const notificationResult = await Notification(notificationOptions);
+  const notificationResult = await Notification(notificationOpts);
   return reply(notificationResult);
 }
 

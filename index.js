929098177505943b685449528e60555f8947e65a
mitogh <mitogh@gmail.com>
2017-05-10 08:53:56 -0500

    Separate routes from the main file

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
-const Reporter = require('./src/Reporter');
-const Joi = require('joi');
 
 server.connection({
   port: (process.env.PORT || 8080),
   host: '0.0.0.0'
 });
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: Reporter,
-  config: {
-    validate: {
-      query: {
-        hipchat: Joi.string().required(),
-        site: Joi.string().uri().required(),
-      }
-    }
-  }
-})
+server.route( require('./routes') );
 
 server.start((error) => {
   if ( error ) {
---
+++ routes.js
@@ -0,0 +1,31 @@
+const Reporter = require('./src/Reporter');
+const Verification = require('./src/Verification');
+const Joi = require('joi');
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/report/',
+    handler: Reporter,
+    config: {
+      validate: {
+        query: {
+          hipchat: Joi.string().required(),
+          site: Joi.string().uri().required(),
+        }
+      }
+    }
+  }, 
+  {
+    method: 'GET',
+    path: '/verification/',
+    handler: Verification,
+    config: {
+      validate: {
+        query: {
+          hipchat: Joi.string().required(),
+        }
+      }
+    }
+  }
+]

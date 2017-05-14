27a929856b6ed61510831323ba9a9adb6c746269
mitogh <mitogh@gmail.com>
2017-05-14 12:55:14 -0500

    Analize site if has a valid URL

--- src/PostReport.js
+++ src/PostReport.js
@@ -1,15 +1,25 @@
+const Reporter = require('./Reporter');
 var validUrl = require('valid-url')
 
-function PostReporter( request, reply ) {
+async function PostReporter( request, reply ) {
   let { item } = request.payload;
   let { message, room } = item || {};
 
   const roomID = 'id' in room ? room.id : 0;
   let chatRequest = 'message' in message ? message.message : '';
-  chatRequest = chatRequest.replace('/weather', '').trim();
+  chatRequest = chatRequest.replace('/report', '').trim();
   console.log('Notification will be done in: ', roomID);
   console.log('Validating: ', chatRequest);
   const isValid = validUrl.isUri( chatRequest );
+
+  if ( isValid ) {
+    await Reporter({
+      query: {
+        site: chatRequest,
+          hipchat: roomID,
+      }
+    }, () => {});
+  }
 
   return reply({
     color: isValid ? 'green' : 'red',

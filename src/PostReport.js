c2d7baa8feadc9fd5d74de424b2e2c4bc45873c0
mitogh <mitogh@gmail.com>
2017-05-14 12:46:07 -0500

    Reply based on the request send

--- src/PostReport.js
+++ src/PostReport.js
@@ -1,8 +1,19 @@
+var validUrl = require('valid-url')
+
 function PostReporter( request, reply ) {
-  console.log(request.payload);
+  let { item } = request.payload;
+  let { message, room } = item || {};
+
+  const roomID = 'id' in room ? room.id : 0;
+  let chatRequest = 'message' in message ? message.message : '';
+  chatRequest = chatRequest.replace('/weather', '').trim();
+  console.log('Notification will be done in: ', roomID);
+  console.log('Validating: ', chatRequest);
+  const isValid = validUrl.isUri( chatRequest );
+
   return reply({
-    color: 'green',
-    message: 'Request recived',
+    color: isValid ? 'green' : 'red',
+    message: isValid ? 'Report recieved, thank you!' : 'Make sure you have a valid URL to analize.',
     notify: false,
     message_format: 'text',
   })

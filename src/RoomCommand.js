47d23a710d387412f15e8f8c1a7c437831735c67
mitogh <mitogh@gmail.com>
2017-05-14 13:20:48 -0500

    Rename into a RoomCommand instead

--- src/RoomCommand.js
+++ src/RoomCommand.js
@@ -1,32 +1,34 @@
 const Reporter = require('./Reporter');
 var validUrl = require('valid-url')
+const errorResponse = {
+  color: 'red',
+  message: 'Make sure you have a valid URL to analize.',
+  notify: false,
+  message_format: 'text',
+}
 
-async function PostReporter( request, reply ) {
+async function RoomCommand( request, reply ) {
   let { item } = request.payload;
   let { message, room } = item || {};
 
   const roomID = 'id' in room ? room.id : 0;
   let chatRequest = 'message' in message ? message.message : '';
   chatRequest = chatRequest.replace('/report', '').trim();
+
   console.log('Notification will be done in: ', roomID);
   console.log('Validating: ', chatRequest);
   const isValid = validUrl.isUri( chatRequest );
 
+  let response = {};
   if ( isValid ) {
     await Reporter({
-      query: {
-        site: chatRequest,
-          hipchat: roomID,
-      }
-    }, () => {});
+      site: chatRequest,
+      hipchat: roomID,
+    });
+  } else {
+    response = errorResponse;
   }
-
-  return reply({
-    color: isValid ? 'green' : 'red',
-    message: isValid ? 'Report recieved, thank you!' : 'Make sure you have a valid URL to analize.',
-    notify: false,
-    message_format: 'text',
-  })
+  return reply(response);
 }
 
-module.exports = PostReporter;
+module.exports = RoomCommand;
